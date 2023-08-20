e59029d10a384e371cb99cbf56738cf7ff895047
BorisPoloyko <boris.poloiko@gmail.com>
2023-08-20 20:24:27 +0300

    feat: add owner name

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Made with love by Boris Poloyko</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
